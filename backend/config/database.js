30471772ef39b9cb271746803abe2c3bb4119fbc
maristizabalo <84209839+maristizabalo@users.noreply.github.com>
2025-03-06 19:26:02 -0500

    Eliminando node y configurando python para backend

--- backend/config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("KrocoBilliard_db", "root", "root", {
-    host: "localhost",
-    dialect: "mysql",
-});
-
-module.exports = sequelize;
