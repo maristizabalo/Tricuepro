bd3cffcf4211ad3d5889a6491ccbbe4d3129c52c
maristizabalo <84209839+maristizabalo@users.noreply.github.com>
2025-03-21 02:38:34 -0500

    incluyendo migrations en el gitignore

--- backend/apps/Billiard/models.py
+++ backend/apps/Billiard/models.py
@@ -4,8 +4,9 @@ from apps.account.models import CustomUser
 class Billiard(models.Model):
     nombre = models.CharField(max_length=100)
     direccion = models.TextField()
-    propietarios = models.ManyToManyField(CustomUser, related_name="billares", limit_choices_to={'rol': 'owner'})
-    admin = models.OneToOneField(CustomUser, on_delete=models.SET_NULL, null=True, blank=True, related_name="administra", limit_choices_to={'rol': 'admin'})
+    propietarios = models.ManyToManyField(CustomUser, related_name="billares")
+    admin = models.OneToOneField(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
+    
 
     def __str__(self):
         return self.nombre
